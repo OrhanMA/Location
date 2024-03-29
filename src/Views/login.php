8f18cb1c889977a96aaf8efff77c8032a21631cb
OrhanMA <orhan.madi.assani@gmail.com>
2024-03-29 13:32:39 +0100

    suppression des fausses données de pré-remplissage des champs de formulaires

--- src/Views/login.php
+++ src/Views/login.php
@@ -8,11 +8,11 @@
   <form action="/location/public/login" method="post">
     <div class="form-field">
       <label for="email">Email</label>
-      <input required type="email" name="email" id="email" value="test@test.com">
+      <input required type="email" name="email" id="email">
     </div>
     <div class="form-field">
       <label for="password">Password</label>
-      <input required type="password" name="password" id="password" value="test1234">
+      <input required type="password" name="password" id="password">
     </div>
     <input class="button bg-accent" type="submit" value="Connexion">
   </form>
--- src/Views/register.php
+++ src/Views/register.php
@@ -8,27 +8,27 @@
   <form action="/location/public/register" method="post">
     <div class='form-field'>
       <label for="first_name">First name</label>
-      <input type="text" minlength="3" maxlength="50" name="first_name" id="first_name" value="Orhan">
+      <input type="text" minlength="3" maxlength="50" name="first_name" id="first_name">
     </div>
     <div class='form-field'>
       <label for="last_name">Last name</label>
-      <input required type="text" minlength="3" maxlength="50" name="last_name" id="last_name" value="Madi Assani">
+      <input required type="text" minlength="3" maxlength="50" name="last_name" id="last_name">
     </div>
     <div class='form-field'>
       <label for="phone">Phone</label>
-      <input required type="phone" name="phone" id="phone" minlength="10" maxlength=10 value="0123456789">
+      <input required type="phone" name="phone" id="phone" minlength="10" maxlength=10>
     </div>
     <div class='form-field'>
       <label for="email">Email</label>
-      <input required type="email" minlength="5" maxlength="80" name="email" id="email" value="test@test.com">
+      <input required type="email" minlength="5" maxlength="80" name="email" id="email">
     </div>
     <div class='form-field'>
       <label for="password">Password</label>
-      <input required type="password" minlength="7" name="password" id="password" value="test1234">
+      <input required type="password" minlength="7" name="password" id="password">
     </div>
     <div class='form-field'>
       <label for="confirm_password">Confirm Password</label>
-      <input required type="password" minlength="7" name="confirm_password" id="confirm_password" value="test1234">
+      <input required type="password" minlength="7" name="confirm_password" id="confirm_password">
     </div>
     <input class="button bg-accent" type="submit" value="Inscription">
   </form>
